d59c4e18c8ac621b859de1e6a0693e6c71633aa4
gfpkelly1986 <gfpkelly@gmail.com>
2022-08-27 13:19:38 +0000

    Add comments and console logs for revision

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,16 +16,33 @@ function processOptions(form) {
             optArray.push(entry[1]);
         }
     }
+    console.log(optArray)
+    // The formData object has form.delete and form.append methods.
+    for (let entry of form.entries()) {
+        console.log(entry)
+    }
     form.delete('options');
+
+    for (let entry2 of form.entries()) {
+        console.log('Entry before appending option to form ' + entry2)
+    }
 
     form.append('options', optArray.join());
 
+    for (let entry3 of form.entries()) {
+        console.log(entry3)
+    }
     return form;
 }
 
 async function postForm(e) {
-    const form = new FormData(document.getElementById('checksform'));
-
+    const form = processOptions(new FormData(document.getElementById('checksform')));
+    // This is code loops through the form object to get its entries() values.
+    /*
+    for (let entry of form.entries()) {
+        console.log(entry)
+    }
+    */
     const response = await fetch(API_URL, {
                         method: "POST",
                         headers: {
@@ -33,9 +50,9 @@ async function postForm(e) {
                                  },
                         body: form,
                         })
-
+                        
     const data = await response.json();
-
+                        
     if (response.ok) {
         displayErrors(data);
     } else {
@@ -69,6 +86,7 @@ function displayErrors(data) {
 
 // Asynchronous functions that make a request to the API.
 async function getStatus(e) {
+    // Thsi line is a GET request to the API server: Its composed of a URL and an API key which pings the server with a request
     const queryString = `${API_URL}?api_key=${API_KEY}`;
 
     const response = await fetch(queryString);
@@ -98,8 +116,8 @@ function displayStatus(data) {
 function displayException(data) {
     let heading = `An Exception Occurred`;
 
-    results = `<div>The API returned status code ${data.status_code}</div>`;
-    results += `<div>Error number <strong>${data.error_no}</strong></div>`;
+    results = `<div>The API returned status code: <strong>${data.status_code}</strong></div>`;
+    results += `<div>Error number: <strong>${data.error_no}</strong></div>`;
     results += `<div>Error text: <strong>${data.error}</strong></div>`;
 
     document.getElementById('resultsModalTitle').innerText = heading;
@@ -108,3 +126,5 @@ function displayException(data) {
     resultsModal.show();
 
 }
+
+
